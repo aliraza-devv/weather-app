19b476b94a1ae9edaefb1ff5ca386b1b8fae4ede
Ali Raza <aliraza.dev.web@gmail.com>
2022-11-29 18:17:25 +0500

    added structure

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import coldBg from './assets/Cold.jpg'
 
 function App() {
   return (
-    <div className="App" style={{ backgroundImage: `url(${hotBg})` }}>
+    <div className="app" style={{ backgroundImage: `url(${hotBg})` }}>
       <div className='overlay'>
         <div className='container'>
-          
+          <div className='section section__inputs'>
+            <input type='text' name='City' placeholder='Enter City...' />
+            <button>°F</button>
+          </div>
+          <div className='section section__temperature'>
+            <div className='icon'>
+              <h3>London</h3>
+              <img src='' alt='Weather Icon' />
+              <h3>Cloudy</h3>
+            </div>
+            <div className='temperature'>
+              <h1>34 °C</h1>
+            </div>
+          </div>
+          {/* Bottom Description */}
         </div>
       </div>
     </div>
