76125df1608d76d16eead755a520efaf02cf388c
Ali Raza <aliraza.dev.web@gmail.com>
2022-11-29 19:09:05 +0500

    displaying data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import hotBg from "./assets/Hot.jpg";
 import coldBg from "./assets/Cold.jpg";
 import Descriptions from "./components/Descriptions.jsx";
 import { getFormattedWeatherData } from "./WeatherService";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState('metric')
+
   useEffect(() => {
     const fetchWeatherData = async () => {
-      const data = await getFormattedWeatherData("paris");
+      const data = await getFormattedWeatherData("paris", units);
+      setWeather(data);
     };
     fetchWeatherData();
   }, []);
@@ -15,25 +19,27 @@ function App() {
   return (
     <div className="app" style={{ backgroundImage: `url(${hotBg})` }}>
       <div className="overlay">
-        <div className="container">
-          <div className="section section__inputs">
-            <input type="text" name="City" placeholder="Enter City..." />
-            <button>°F</button>
-          </div>
-          <div className="section section__temperature">
-            <div className="icon">
-              <h3>London</h3>
-              <img src="" alt="Weather Icon" />
-              <h3>Cloudy</h3>
+        {weather && (
+          <div className="container">
+            <div className="section section__inputs">
+              <input type="text" name="City" placeholder="Enter City..." />
+              <button>°F</button>
             </div>
-            <div className="temperature">
-              <h1>34 °C</h1>
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>{`${weather.name}, ${weather.country}`}</h3>
+                <img src={weather.iconURL} alt="Weather Icon" />
+                <h3>{weather.description}</h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${weather.temp.toFixed()} °${units === 'metric' ? 'C' : 'F'}`}</h1>
+              </div>
             </div>
-          </div>
 
-          {/* Bottom Description */}
-          <Descriptions />
-        </div>
+            {/* Bottom Description */}
+            <Descriptions weather={weather} units={units}/>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/Descriptions.jsx
+++ src/components/Descriptions.jsx
@@ -1,33 +1,24 @@
-import React from 'react';
-import './Descriptions.css'
-import { FaArrowDown } from 'react-icons/fa'
+import React from "react";
+import "./Descriptions.css";
+import { FaArrowUp, FaArrowDown, FaWind } from "react-icons/fa";
+import { BiHappy } from "react-icons/bi";
+import { MdCompress, MdOutlineWaterDrop } from "react-icons/md";
 
-const Descriptions = () => {
+const Descriptions = ({ weather, units }) => {
+
+
   return (
-    <div className='section section__descriptions'>
-        <div className='card'>
-            <div className='description__card-icon'>
-                <FaArrowDown />
-                <small>min</small>
-            </div>
-            <h2>32 °C</h2>
+    <div className="section section__descriptions">
+    
+      <div className="card">
+        <div className="description__card-icon">
+          <FaArrowDown />
+          <small>min</small>
         </div>
-        <div className='card'>
-            <div className='description__card-icon'>
-                <FaArrowDown />
-                <small>min</small>
-            </div>
-            <h2>32 °C</h2>
-        </div>
-        <div className='card'>
-            <div className='description__card-icon'>
-                <FaArrowDown />
-                <small>min</small>
-            </div>
-            <h2>32 °C</h2>
-        </div>
+        <h2>32 °C</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Descriptions
+export default Descriptions;
