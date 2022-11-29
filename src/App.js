d320d757a367f2dd7c3dda2ced110edb2e72d3ce
Ali Raza <aliraza.dev.web@gmail.com>
2022-11-29 18:05:53 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import hotBg from './assets/Hot.jpg'
+import coldBg from './assets/Cold.jpg'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(${hotBg})` }}>
+      <div className='overlay'>
+        <div className='container'>
+          
+        </div>
+      </div>
     </div>
   );
 }
